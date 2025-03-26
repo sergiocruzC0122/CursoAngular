d5b85f1dfaa510b42dc4e973c8608823b789e47e
sergiocruzC0122 <thiorfthix@gmail.com>
2025-03-26 12:11:37 -0500

    Agregando nuevo personaje

--- 01-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 01-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal,computed } from '@angular/core';
 
 interface Character {
   id: number;
@@ -8,14 +9,30 @@ interface Character {
 
 @Component({
   templateUrl: './dragonball-page.component.html',
+  // imports: [NgClass],
 })
 
 export class DragonballPageComponent {
+
+  name = signal('Gohan');
+  power= signal(100);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001},
     { id: 2, name: 'Vegeta', power: 8000},
     { id: 3, name: 'Picoro', power: 3000},
+    { id: 4, name: 'Yamcha', power: 500},
   ]);
+
+  addCharacter(){
+    console.log(this.name(), this.power());
+  };
+
+
+  // powerClasses = computed(() =>{
+  //   return{
+  //     'text-danger':true,
+  //   };
+  // });
 
 }
