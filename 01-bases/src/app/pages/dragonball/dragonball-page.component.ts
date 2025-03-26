1516fe523735d242886bb95b0a652180ff93815f
sergiocruzC0122 <thiorfthix@gmail.com>
2025-03-26 14:05:39 -0500

    Culminacion Pagina DragonBall

--- 01-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 01-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,5 +1,4 @@
-import { NgClass } from '@angular/common';
-import { Component, signal,computed } from '@angular/core';
+import { Component, signal} from '@angular/core';
 
 interface Character {
   id: number;
@@ -20,19 +19,30 @@ export class DragonballPageComponent {
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001},
     { id: 2, name: 'Vegeta', power: 8000},
-    { id: 3, name: 'Picoro', power: 3000},
     { id: 4, name: 'Yamcha', power: 500},
+    { id: 3, name: 'Picoro', power: 3000},
   ]);
 
   addCharacter(){
-    console.log(this.name(), this.power());
-  };
+    if(!this.name() || !this.power() || this.power() <= 0){
+      return;
+    }
 
+    const newCharacter: Character = {
+      id: this.characters().length +1,
+      name: this.name(),
+      power: this.power(),
+    };
 
-  // powerClasses = computed(() =>{
-  //   return{
-  //     'text-danger':true,
-  //   };
-  // });
+    this.characters.update((list) => [... list, newCharacter]);
+    this.resetFields();
+
+  }
+
+  resetFields(){
+    this.name.set('');
+    this.power.set(0);
+  }
+
 
 }
