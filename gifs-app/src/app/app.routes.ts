2210eb187e590677a7a58cf50d899a004311b374
sergiocruzC0122 <thiorfthix@gmail.com>
2025-03-27 14:32:35 -0500

    Creacion de componente Html

--- gifs-app/src/app/app.routes.ts
+++ gifs-app/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () =>
+            import('./gift/pages/dashboard-page/dashboard-page.component'),
+
+        children:[
+            {
+                path: 'trending',
+                loadComponent: () =>
+                    import('./gift/pages/trending-page/trending-page.component'),
+            },
+            {
+                path: 'search',
+                loadComponent: () =>
+                    import('./gift/pages/search-page/search-page.component'),
+            },
+
+            {
+                path: '**',
+                redirectTo: 'trending',
+            },
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+    },
+];
+
---
+++ gifs-app/src/app/gift/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,9 @@
+import { RouterOutlet } from '@angular/router';
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [RouterOutlet],
+  templateUrl: './dashboard-page.component.html',
+})
+export default class DashboardPageComponent { }
